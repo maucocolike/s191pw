a7d380b4827b94b63c4edc1600a02a620386e9be
maucocolike <122042514@upq.edu.mx>
2024-09-25 20:33:38 -0600

    avance practica 3

---
+++ maucoco1/resources/views/formulario.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    @vite(['resources/js/app.js'])
+    <title>Registro clientes</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Turista sin maps</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Formulario de registro <span class="sr-only">(current)</span></a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="#">Consultar clientes</a>
+      </li>
+
+    </ul>
+  </div>
+</nav>
+</body>
+</html>
--- maucoco1/routes/web.php
+++ maucoco1/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
+
+Route::view('/','welcome');
+
+Route::view('/formulario','formulario');
